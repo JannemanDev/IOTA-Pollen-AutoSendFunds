9933c2f98ffdb9f46a9fabb98f1862de026d6d9e
Jan Oonk <J.Oonk@fontys.nl>
2021-03-29 13:33:48 +0200

    .

--- buildAll.bat
+++ buildAll.bat
@@ -1,7 +1,7 @@
-cd /d "C:\MyData\Persoonlijk\IOTA\source\IOTA-Pollen-AutoSendFunds"
+cd /d "C:\MyData\Persoonlijk\IOTA\IOTA-Pollen-AutoSendFunds"
 
-dotnet publish source\IOTA-Pollen-AutoSendFunds\IOTA-Pollen-AutoSendFunds.csproj /p:PublishProfile=source\Properties\PublishProfiles\linux-arm.pubxml
-dotnet publish source\IOTA-Pollen-AutoSendFunds\IOTA-Pollen-AutoSendFunds.csproj /p:PublishProfile=source\Properties\PublishProfiles\Linux-x64.pubxml
-dotnet publish source\IOTA-Pollen-AutoSendFunds\IOTA-Pollen-AutoSendFunds.csproj /p:PublishProfile=source\Properties\PublishProfiles\osx-x64.pubxml
-dotnet publish source\IOTA-Pollen-AutoSendFunds\IOTA-Pollen-AutoSendFunds.csproj /p:PublishProfile=source\Properties\PublishProfiles\win-x64.pubxml
-dotnet publish source\IOTA-Pollen-AutoSendFunds\IOTA-Pollen-AutoSendFunds.csproj /p:PublishProfile=source\Properties\PublishProfiles\win-x86.pubxml
+rem dotnet publish source\IOTA-Pollen-AutoSendFunds\Dashboard-AutoSendFunds.csproj /p:PublishProfile=source\Properties\PublishProfiles\linux-arm.pubxml
+dotnet publish source\IOTA-Pollen-AutoSendFunds\Dashboard-AutoSendFunds.csproj /p:PublishProfile=source\Properties\PublishProfiles\Linux-x64.pubxml
+rem dotnet publish source\IOTA-Pollen-AutoSendFunds\Dashboard-AutoSendFunds.csproj /p:PublishProfile=source\Properties\PublishProfiles\osx-x64.pubxml
+rem dotnet publish source\IOTA-Pollen-AutoSendFunds\Dashboard-AutoSendFunds.csproj /p:PublishProfile=source\Properties\PublishProfiles\win-x64.pubxml
+rem dotnet publish source\IOTA-Pollen-AutoSendFunds\Dashboard-AutoSendFunds.csproj /p:PublishProfile=source\Properties\PublishProfiles\win-x86.pubxml
